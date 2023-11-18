c0b5e952eb129aa57ee0a5f43f985575d9fe7196
Ionut Gherman <ghermanionut96@gmail.com>
2023-11-18 14:26:38 +0200

    Initial walleye firmware commit

---
+++ config.mk
@@ -0,0 +1,16 @@
+FIRMWARE_IMAGES := \
+    abl \
+    apdp \
+    cmnlib \
+    cmnlib64 \
+    devcfg \
+    hyp \
+    keymaster \
+    modem \
+    msadp \
+    pmic \
+    rpm \
+    tz \
+    xbl
+
+AB_OTA_PARTITIONS += $(FIRMWARE_IMAGES)
